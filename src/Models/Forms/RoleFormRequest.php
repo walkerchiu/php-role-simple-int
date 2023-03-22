772cd87af018c7a78768959b5a9b43a0d9843b7c
walkerchiu <chenjen.chiou@gmail.com>
2023-03-22 20:38:17 +0800

    Chore(Vendor): Rename "php-site" to "php-site-mall"

--- src/Models/Forms/RoleFormRequest.php
+++ src/Models/Forms/RoleFormRequest.php
@@ -114,11 +114,11 @@ class RoleFormRequest extends FormRequest
                 && isset($data['host_id'])
             ) {
                 if (
-                    config('wk-role-simple.onoff.site')
-                    && !empty(config('wk-core.class.site.site'))
-                    && $data['host_type'] == config('wk-core.class.site.site')
+                    config('wk-role-simple.onoff.site-mall')
+                    && !empty(config('wk-core.class.site-mall.site'))
+                    && $data['host_type'] == config('wk-core.class.site-mall.site')
                 ) {
-                    $result = DB::table(config('wk-core.table.site.sites'))
+                    $result = DB::table(config('wk-core.table.site-mall.sites'))
                                  ->where('id', $data['host_id'])
                                  ->exists();
                     if (!$result)
--- src/config/role-simple.php
+++ src/config/role-simple.php
@@ -32,7 +32,7 @@ return [
         'morph-image'    => 0,
         'rule'           => 0,
         'rule-hit'       => 0,
-        'site'           => 0
+        'site-mall'      => 0,
     ],
 
     /*
